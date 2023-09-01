2e55e4dcc538ed15d8490795808ae040539a1197
Dmartins2695 <62670499+Dmartins2695@users.noreply.github.com>
2023-09-01 09:10:44 +0100

    feat: amplify initial config

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "budgetapp66594dce": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
